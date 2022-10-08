2a9edb3ff76327083452fb4f558b18999608aa0e
AlexandreSh <113853602+AlexandreSh@users.noreply.github.com>
2022-10-08 14:39:40 -0400

    inicioa

--- VHDL/p1/ex1.vhd
+++
@@ -1,9 +0,0 @@
-library ieee;
-use iee.std_logic_1164.all
-entity adder1 is
-    port (
-        
-    )
-
-    result <= a xor b xor carryin
-    carryout <= (a and b) or (a and carryin) or (c and carryin)
