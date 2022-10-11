ee34feaf910ab2c47ece96db07cc847c16743e54
alex <alexandre.shimizu@ufms.br>
2022-10-11 15:17:57 -0400

    Nada funciona

--- codec.vhd
+++ codec.vhd
@@ -20,38 +20,39 @@ architecture behavioral of codec is
 begin 
     process(interrupt)
         subtype dados1byte is std_logic_vector(7 downto 0); 
-        type arquivo is file of dados1byte;
+        type entrada_codec is file of dados1byte;
+        type saida_codec is file of dados1byte;
         variable aux1b          : dados1byte; --variavel auxiliar que guarda os dados
-        file arq_carga          : arquivo; --!!!!!substituir pelo nome de arquivo que usaremos
-        variable d1byte         : dados1byte; 
-        variable nome_arquivo   : string(1 to 50);
-        variable tam_nome_arq   : natural;
+        file arq_carga_in       : entrada_codec open read_mode is "/caminho/do/codec/entrada_codec.dat";
+        file arq_carga_out      : saida_codec open write_mode is "/caminho/do/codec/entrada_codec.dat";
+  --      variable nome_arquivo   : string(1 to 50);
+  --      variable tam_nome_arq   : natural;
         variable status         : file_open_status;
         begin
 
-
             if ((read_signal = '1')and(write_signal = '0')) then      --instrução IN (read)
-                file_open(status, arq_carga, nome_arquivo(1 to tam_nome_arq), read_mode);
+                file_open(status, arq_carga_in, "/caminho/do/codec/entrada_codec.dat", read_mode);
                 if status /= open_ok then
                     report "ERRO NA LEITURA DO ARQUIVO" severity warning;
-                else
-                    read(arq_carga, aux1b);  --lê o arquivo e salva na variavel auxiliar
+                elsif not endfile(arq_carga_in) then
+                    read(arq_carga_in, aux1b);  --lê o arquivo e salva na variavel auxiliar
                     codec_data_out <= aux1b; --disponibiliza os dados na variavel de saída
                     valid <= '1';            --sinaliza leitura com sucesso
                 end if;
+                file_close(arq_carga_in);
             elsif ((read_signal = '0')and(write_signal = '1')) then   --instrução OUT (write)
-                file_open(status, arq_carga, nome_arquivo(1 to tam_nome_arq), write_mode);
+                file_open(status, arq_carga_out, "/caminho/do/codec/entrada_codec.dat", write_mode);
                 if status /= open_ok then
-                    report "ERRO NA LEITURA DO ARQUIVO" severity warning;
+                    report "ERRO NA ESCRITA DO ARQUIVO" severity warning;
                 else
                     aux1b := codec_data_in;
-                    write(arq_carga, aux1b); --escreve os dados de aux1b no arquivo
+                    write(arq_carga_out, aux1b); --escreve os dados de aux1b no arquivo
                     valid <= '1'; --sinaliza escrita com sucesso
                 end if;
+                file_close(arq_carga_out);
             else
                 valid <= '0';                                       --em caso de pedido não esperado
             end if;
-
 
         end process;
 
