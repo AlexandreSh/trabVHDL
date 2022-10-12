f78c5337f9b5cc4d6319a18899e7c27ef7e72e65
alex <alexandre.shimizu@ufms.br>
2022-10-12 15:26:40 -0400

    Codec e TB_codec conseguem ler arquivos mas nao escrever

--- codec.vhd
+++ codec.vhd
@@ -1,5 +1,8 @@
 library ieee, std;
 use ieee.std_logic_1164.all;
+use IEEE.std_logic_textio.all;
+use ieee.std_logic_arith.all;
+use ieee.numeric_std.all;
 use std.textio.all;
 
 entity codec is
@@ -15,46 +18,32 @@ codec_data_in : in std_logic_vector(7 downto 0);
 codec_data_out : out std_logic_vector(7 downto 0)
 );
 end entity;
---como sabemos que arquivo iremos ler? ele tem só um byte? 
 architecture behavioral of codec is
 begin 
     process(interrupt)
-        subtype dados1byte is std_logic_vector(7 downto 0); 
-        type entrada_codec is file of dados1byte;
-        type saida_codec is file of dados1byte;
-        variable aux1b          : dados1byte; --variavel auxiliar que guarda os dados
-        file arq_carga_in       : entrada_codec open read_mode is "/caminho/do/codec/entrada_codec.dat";
-        file arq_carga_out      : saida_codec open write_mode is "/caminho/do/codec/entrada_codec.dat";
-  --      variable nome_arquivo   : string(1 to 50);
-  --      variable tam_nome_arq   : natural;
-        variable status         : file_open_status;
-        begin
-
-            if ((read_signal = '1')and(write_signal = '0')) then      --instrução IN (read)
-                file_open(status, arq_carga_in, "/caminho/do/codec/entrada_codec.dat", read_mode);
-                if status /= open_ok then
-                    report "ERRO NA LEITURA DO ARQUIVO" severity warning;
-                elsif not endfile(arq_carga_in) then
-                    read(arq_carga_in, aux1b);  --lê o arquivo e salva na variavel auxiliar
-                    codec_data_out <= aux1b; --disponibiliza os dados na variavel de saída
-                    valid <= '1';            --sinaliza leitura com sucesso
-                end if;
-                file_close(arq_carga_in);
-            elsif ((read_signal = '0')and(write_signal = '1')) then   --instrução OUT (write)
-                file_open(status, arq_carga_out, "/caminho/do/codec/entrada_codec.dat", write_mode);
-                if status /= open_ok then
-                    report "ERRO NA ESCRITA DO ARQUIVO" severity warning;
-                else
-                    aux1b := codec_data_in;
-                    write(arq_carga_out, aux1b); --escreve os dados de aux1b no arquivo
-                    valid <= '1'; --sinaliza escrita com sucesso
-                end if;
-                file_close(arq_carga_out);
+        file arq_entrada: text is "/home/alex/Documents/LabHardware/T1/trabVHDL/entrada_codec.dat";
+        file arq_saida: text is "/home/alex/Documents/LabHardware/T1/trabVHDL/saida_codec.dat";
+        variable linha_dados_in: line;
+        variable linha_dados_out: line;
+        variable aux: std_logic_vector(7 downto 0);
+    begin
+        if ((read_signal = '1')and(write_signal = '0')) then      --instrução IN (read)
+            if not endfile(arq_entrada) then
+                readline(arq_entrada, linha_dados_in);  --lê o arquivo e salva na variavel auxiliar
+                hread(linha_dados_in, aux);
+                codec_data_out <= std_logic_vector(aux); --disponibiliza os dados na variavel de saída
+                valid <= '1';            --sinaliza leitura com sucesso
             else
-                valid <= '0';                                       --em caso de pedido não esperado
+                report "ERRO NA LEITURA DO ARQUIVO" severity warning;
             end if;
+        elsif ((read_signal = '0')and(write_signal = '1')) then   --instrução OUT (write)
+            hwrite(linha_dados_out, codec_data_out);
+--            linha_dados_out := to_string(codec_data_in);
+            writeline(arq_saida, linha_dados_out); --escreve os dados de aux1b no arquivo
+            valid <= '1'; --sinaliza escrita com sucesso
+        else
+            valid <= '0';                                       --em caso de pedido não esperado
+        end if;
 
         end process;
-
-
 end behavioral;
--- tb_codec.vhd
+++ tb_codec.vhd
@@ -1,11 +1,14 @@
-library ieee;
+library ieee, std;
 use ieee.std_logic_1164.all;
+use IEEE.std_logic_textio.all;
+use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
+use std.textio.all;
 
-entity tb_codec is
+entity tb_testearquivo is
 end entity;
 
-architecture mixed of tb_codec is
+architecture mixed of tb_testearquivo is
 	subtype dados1byte is std_logic_vector(7 downto 0); --vou precisar de declarar aquivo aqui?
 	signal interrupt, read_signal, write_signal, valid: std_logic;
 	signal codec_data_in, codec_data_out: std_logic_vector(7 downto 0);
@@ -16,21 +19,32 @@ begin
 		port map(interrupt, read_signal, write_signal, valid, codec_data_in, codec_data_out);
 
 	estimulo: process is
+        variable L: line;
+        file arq_entrada: text is "/home/alex/Documents/LabHardware/T1/trabVHDL/entrada_codec.dat";
+        file arq_saida: text is "/home/alex/Documents/LabHardware/T1/trabVHDL/saida_codec.dat";
 	--	variable resultado: dados1byte;
 		type tab_ver is record
 			interrupt, read_signal, write_signal, valid: std_logic;
-			codec_data_in, codec_data_out: std_logic_vector(7 downto 0); --onde é que eu coloco os arquivosssS??????
+			codec_data_in, codec_data_out: std_logic_vector(7 downto 0); 
 		end record;
-		type vet_tab_ver is array (0 to 7) of tab_ver;
+		type vet_tab_ver is array (0 to 15) of tab_ver;
 		constant tabela_verdade: vet_tab_ver := (
 	--	interrupt, read_signal, write_signal, valid, codec_data_in, codec_data_out	
-		(	'1'	,	'1'		  ,		'0'		,	'1'	,"00000000"	,  "11111111"	), --Não faço a mínima ideia como validar arquivos, enviei email pro professor pedindo atendimento e estou aguardando resposta
 		(	'1'	,	'1'		  ,		'0'		,	'1'	,"00000000"	,  "11111111"	),
+		(	'0'	,	'1'		  ,		'0'		,	'1'	,"00000000"	,  "11111111"	),
+		(	'1'	,	'1'		  ,		'0'		,	'1'	,"00000000"	,  "11111111"	),
+		(	'0'	,	'1'		  ,		'0'		,	'1'	,"00000000"	,  "11111111"	),
 		(	'1'	,	'0'		  ,		'1'		,	'1'	,"11111111"	,  "00000000"	),
+		(	'0'	,	'0'		  ,		'1'		,	'1'	,"11111111"	,  "00000000"	),
 		(	'1'	,	'0'		  ,		'1'		,	'1'	,"11111111"	,  "00000000"	),
+		(	'0'	,	'0'		  ,		'1'		,	'1'	,"11111111"	,  "00000000"	),
 		(	'1'	,	'1'		  ,		'1'		,	'0'	,"11111111"	,  "00000000"	), --daqui pra baixo testa entradas invalidas
-		(	'1'	,	'0'		  ,		'0'		,	'0'	,"11111111"	,  "00000000"	),
 		(	'0'	,	'1'		  ,		'1'		,	'0'	,"11111111"	,  "00000000"	),
+		(	'1'	,	'0'		  ,		'0'		,	'0'	,"11111111"	,  "00000000"	),
+		(	'0'	,	'0'		  ,		'0'		,	'0'	,"11111111"	,  "00000000"	),
+		(	'1'	,	'1'		  ,		'1'		,	'0'	,"11111111"	,  "00000000"	),
+		(	'0'	,	'1'		  ,		'1'		,	'0'	,"11111111"	,  "00000000"	),
+		(	'1'	,	'0'		  ,		'0'		,	'0'	,"11111111"	,  "00000000"	),
 		(	'0'	,	'0'		  ,		'0'		,	'0'	,"11111111"	,  "00000000"	)		);
 
 		begin 
@@ -39,10 +53,21 @@ begin
 				read_signal		<=tabela_verdade(i).read_signal;
 				write_signal	<=tabela_verdade(i).write_signal;
 				wait for 1 ns;
+                deallocate(L);
 				if i<2 then 			--valida dataout
-					assert codec_data_out = tabela_verdade(i).codec_data_out report "ERRO NO DATAOUT";
+                    write(L, string'("ERRO NO DATAOUT "));
+                    write(L, codec_data_out);
+                    write(L, string'(" recebido mas "));
+                    write(L, tabela_verdade(i).codec_data_out);
+                    write(L, string'(" esperado "));
+					assert codec_data_out = tabela_verdade(i).codec_data_out report L.all;
 				elsif i>=2 and i<5 then--valida datain
-					assert codec_data_in = tabela_verdade(i).codec_data_in report "ERRO NO DATAIN";
+                    write(L, string'("ERRO NO DATAIN "));
+                    write(L, codec_data_out);
+                    write(L, string'(" recebido mas "));
+                    write(L, tabela_verdade(i).codec_data_out);
+                    write(L, string'(" esperado "));
+					assert codec_data_in = tabela_verdade(i).codec_data_in report l.all;
 				end if;
 				assert valid = tabela_verdade(i).valid report "ERRO NA VALIDACAO";
 			end loop;
