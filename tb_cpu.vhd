c681da53c4a3b41909165683d30e3cb370378b54
RaphaelaCabrera <raphaeladbc@hotmail.com>
2022-11-15 17:41:54 -0400

    Iniciados os testbenches das entidades soc e cpu.

---
+++ tb_cpu.vhd
@@ -0,0 +1,9 @@
+library ieee, std;
+use ieee.std_logic_1164.all;
+use IEEE.std_logic_textio.all;
+use ieee.std_logic_arith.all;
+use ieee.numeric_std.all;
+use std.textio.all;
+
+entity tb_cpu is
+end entity;
