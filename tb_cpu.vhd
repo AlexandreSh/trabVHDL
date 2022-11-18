c101e0e6c3d024a61924bf41340f6677974b3cb4
alex <alexandre.shimizu@ufms.br>
2022-11-17 23:05:47 -0400

    TBs gerados pela ferramenta

--- tb_cpu.vhd
+++ tb_cpu.vhd
@@ -1,9 +1,90 @@
-library ieee, std;
+library ieee;
 use ieee.std_logic_1164.all;
-use IEEE.std_logic_textio.all;
-use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
-use std.textio.all;
 
-entity tb_cpu is
-end entity;
+entity cpu_tb is
+end;
+
+architecture bench of cpu_tb is
+
+  component cpu
+    generic (
+      addr_width : natural;
+      data_width : natural
+    );
+      port (
+      clock : in std_logic;
+      halt : in std_logic;
+      instruction_in : in std_logic_vector(data_width-1 downto 0);
+      instruction_addr : out std_logic_vector(addr_width-1 downto 0);
+      data_read : out std_logic;
+      data_write : out std_logic;
+      data_addr : out std_logic_vector(addr_width-1 downto 0);
+      data_in : out std_logic_vector((data_width*4)-1 downto 0);
+      data_out : in std_logic_vector(data_width-1 downto 0);
+      codec_interrupt : out std_logic;
+      codec_read : out std_logic;
+      codec_write : out std_logic;
+      codec_valid : in std_logic;
+      codec_data_out : in std_logic_vector(7 downto 0);
+      codec_data_in : out std_logic_vector(7 downto 0)
+    );
+  end component;
+
+  -- Clock period
+  constant clk_period : time := 5 ns;
+  -- Generics
+  constant addr_width : natural := 16;
+  constant data_width : natural := 8;
+
+  -- Ports
+  signal clock : std_logic;
+  signal halt : std_logic;
+  signal instruction_in : std_logic_vector(data_width-1 downto 0);
+  signal instruction_addr : std_logic_vector(addr_width-1 downto 0);
+  signal data_read : std_logic;
+  signal data_write : std_logic;
+  signal data_addr : std_logic_vector(addr_width-1 downto 0);
+  signal data_in : std_logic_vector((data_width*4)-1 downto 0);
+  signal data_out : std_logic_vector(data_width-1 downto 0);
+  signal codec_interrupt : std_logic;
+  signal codec_read : std_logic;
+  signal codec_write : std_logic;
+  signal codec_valid : std_logic;
+  signal codec_data_out : std_logic_vector(7 downto 0);
+  signal codec_data_in : std_logic_vector(7 downto 0);
+
+begin
+
+  cpu_inst : cpu
+    generic map (
+      addr_width => addr_width,
+      data_width => data_width
+    )
+    port map (
+      clock => clock,
+      halt => halt,
+      instruction_in => instruction_in,
+      instruction_addr => instruction_addr,
+      data_read => data_read,
+      data_write => data_write,
+      data_addr => data_addr,
+      data_in => data_in,
+      data_out => data_out,
+      codec_interrupt => codec_interrupt,
+      codec_read => codec_read,
+      codec_write => codec_write,
+      codec_valid => codec_valid,
+      codec_data_out => codec_data_out,
+      codec_data_in => codec_data_in
+    );
+
+--   clk_process : process
+--   begin
+--   clk <= '1';
+--   wait for clk_period/2;
+--   clk <= '0';
+--   wait for clk_period/2;
+--   end process clk_process;
+
+end;
--- tb_soc.vhd
+++ tb_soc.vhd
@@ -1,9 +1,54 @@
-library ieee, std;
+library ieee;
 use ieee.std_logic_1164.all;
-use IEEE.std_logic_textio.all;
-use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
-use std.textio.all;
 
-entity tb_socs is
-end entity;
+entity soc_tb is
+end;
+
+architecture bench of soc_tb is
+
+  component soc
+    generic (
+      firmware_filename : string;
+      addr_width : natural;
+      data_width : natural
+    );
+      port (
+      clock : in std_logic;
+      started : in std_logic
+    );
+  end component;
+
+  -- Clock period
+  constant clk_period : time := 5 ns;
+  -- Generics
+  constant firmware_filename : string := "firmware.bin";
+  constant addr_width : natural := 16;
+  constant data_width : natural := 8;
+
+  -- Ports
+  signal clock : std_logic;
+  signal started : std_logic;
+
+begin
+
+  soc_inst : soc
+    generic map (
+      firmware_filename => firmware_filename,
+      addr_width => addr_width,
+      data_width => data_width
+    )
+    port map (
+      clock => clock,
+      started => started
+    );
+
+--   clk_process : process
+--   begin
+--   clk <= '1';
+--   wait for clk_period/2;
+--   clk <= '0';
+--   wait for clk_period/2;
+--   end process clk_process;
+
+end;
